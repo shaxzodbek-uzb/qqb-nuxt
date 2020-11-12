27fe50cd222fdc34c30b3781f4b9337e6d658193
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-11-12 10:17:46 +0500

    nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
       {
-        src: 'http://code-ya.jivosite.com/widget/cHXd28Shen',
+        src: 'https://code-ya.jivosite.com/widget/cHXd28Shen',
         body: true,
       },
     ],
