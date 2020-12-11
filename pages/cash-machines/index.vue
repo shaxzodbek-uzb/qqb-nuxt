18f3f966cd0b36e44efab16a8adf99394f83dd2a
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-12-11 11:01:05 +0500

    fix url

--- pages/cash-machines/index.vue
+++ pages/cash-machines/index.vue
@@ -11,7 +11,7 @@
           </router-link>
           <router-link
             class="title-route-items d-flex align-center"
-            :to="localePath('/cash_machines')"
+            :to="localePath('/cash-machines')"
           >
             <div class="route-swich">
               <div
